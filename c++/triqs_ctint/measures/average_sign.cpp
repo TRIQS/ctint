11ff5272ce769f7f6d42c1bd018dec479f90b1f3
Nils Wentzell <nils.wentzell@gmail.com>
2017-11-08 12:51:51 +0100

    Implement new version of alpha tensor
    - Use offdiagonal terms
    - Introduce asymmetry per interaction term
    - Added Jacobimatrix in solver.py and added jacobi_test.py
    - Notes alpha analysis, static / dynamic sign
    
    Co-authored-by: Marcel Klett <m.klett@fkf.mpg.de>

--- c++/triqs_ctint/measures/average_sign.cpp
+++ c++/triqs_ctint/measures/average_sign.cpp
@@ -2,14 +2,31 @@
 
 namespace triqs_ctint::measures {
 
-  average_sign::average_sign(params_t const &, qmc_config_t const &, container_set *results) : average_sign_(results->average_sign) {
+  average_sign::average_sign(params_t const &, qmc_config_t const &qmc_config_, container_set *results)
+     : average_sign_(results->average_sign), qmc_config(qmc_config_) {
     average_sign_ = 0.0;
   }
 
   void average_sign::accumulate(mc_weight_t sign) {
     average_sign_ += sign;
+
+    // mc_sign_t det_static  = 0.0;
+    //mc_sign_t det_dynamic = 0.0;
+
+      //if (params.sign_analysis) {
+      // - Get matrix for each block (qmc_config.dets[bl].matrix();)
+      // - Set elements for equal / non-equal times to zero in the matrix
+      // - Calculate det
+      // - Multiply dets and take the sign
+      //}
     ++count;
   }
+
+  /**
+* add sign_analysis to params -> add only sign measure
+* initialize additional dets in config for static/dynamic sign
+* Update insert / remove to update also other dets
+*/
 
   void average_sign::collect_results(mpi::communicator const &comm) {
     average_sign_ = mpi::all_reduce(average_sign_, comm);
