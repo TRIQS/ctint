fe0594aa5831ac99013fead33904eee6565bb089
marcel-klett <marcel.klett@uni-tuebingen.de>
2019-09-09 11:16:51 +0200

    Added Jacobimatrix in solver.py and added jacobi_test.py

--- python/triqs_ctint/solver.py
+++ python/triqs_ctint/solver.py
@@ -19,6 +19,7 @@ def mpi_print(arg):
 class Solver(SolverCore):
 
     def __init__(self, **constr_params):
+        self.eval_count = 0
         """
         Initialise the solver.
 
@@ -48,6 +49,157 @@ class Solver(SolverCore):
         # Initialise the core solver
         SolverCore.__init__(self, **constr_params)
 
+
+
+    def f(self, x,solve_params):
+        #setting up needed parameters
+        self.eval_count = self.eval_count + 1
+        assert 'h_int' in solve_params
+        if 'n_s' not in solve_params: solve_params['n_s'] = 1
+        if 'delta' not in solve_params: solve_params['delta'] = 0.1
+        solve_params.pop('delta')
+        n_s = solve_params['n_s']
+        h_int = solve_params['h_int']
+        gf_struct = self.constr_params['gf_struct']
+        n_terms = len(list(h_int))
+        km = {}
+        for bl, g_bl in self.G0_iw:
+            self.G0_iw_inv[bl] << inverse(g_bl)
+            km[bl] = make_zero_tail(g_bl, 2)
+            km[bl][1] = np.eye(g_bl.target_shape[0])
+        _alpha = x.reshape(n_terms, 2, 2, n_s)
+        _params = solve_params.copy()
+        _params['alpha'] = _alpha
+        _params.update(self.constr_params)
+        self.prepare_G0_shift_iw(**_params)
+        _G0_shift_dens = { bl: g_bl.density(km[bl]).real for bl, g_bl in self.G0_shift_iw }
+        # Evaluate the violation of the self-consistency condition
+        _res = []
+        for n, (term, coeff) in enumerate(h_int):
+            bl_0, u_cdag_0 = term[0][1]
+            bl_1, u_cdag_1 = term[1][1]
+            bl_1, u_c_1 = term[2][1]
+            bl_0, u_c_0 = term[3][1]
+            idx_cdag_0 = dict(gf_struct)[bl_0].index(u_cdag_0)
+            idx_cdag_1 = dict(gf_struct)[bl_1].index(u_cdag_1)
+            idx_c_1 = dict(gf_struct)[bl_1].index(u_c_1)
+            idx_c_0 = dict(gf_struct)[bl_0].index(u_c_0)
+            _res.append(_G0_shift_dens[bl_0][idx_c_0,idx_cdag_0] - _alpha[n,0,0,0])
+            if bl_0 == bl_1:
+                _res.append(_G0_shift_dens[bl_0][idx_c_1,idx_cdag_0] - _alpha[n,0,1,0])
+                _res.append(_G0_shift_dens[bl_0][idx_c_0,idx_cdag_1] - _alpha[n,1,0,0])
+            else:
+                _res.append(-_alpha[n,0,1,0])
+                _res.append(-_alpha[n,1,0,0])
+            _res.append(_G0_shift_dens[bl_1][idx_c_1,idx_cdag_1] - _alpha[n,1,1,0])
+        return np.array(_res)
+
+    def convert_alpha_to_ij(self,h_int,gf_struct,n,t):
+        U_tilde = 0.0
+        bl = None
+        for m, (term, coeff) in enumerate(h_int):
+            if n == m:
+                bl_0, u_cdag_0 = term[0][1]
+                bl_1, u_cdag_1 = term[1][1]
+                bl_1, u_c_1 = term[2][1]
+                bl_0, u_c_0 = term[3][1]
+                idx_cdag_0 = dict(gf_struct)[bl_0].index(u_cdag_0)
+                idx_cdag_1 = dict(gf_struct)[bl_1].index(u_cdag_1)
+                idx_c_1 = dict(gf_struct)[bl_1].index(u_c_1)
+                idx_c_0 = dict(gf_struct)[bl_0].index(u_c_0)
+                if t == 0:
+                    bl = bl_1
+                    i  = idx_cdag_1
+                    j  = idx_c_1
+                    U_tilde = -coeff
+                elif t == 1:
+                    if bl_0 == bl_1:
+                        bl = bl_1
+                        i  = idx_cdag_1
+                        j  = idx_c_0
+                        U_tilde = coeff
+                    else:
+                        return None
+                elif t == 2:
+                    if bl_0 == bl_1:
+                        bl = bl_1
+                        i  = idx_cdag_0
+                        j  = idx_c_1
+                        U_tilde = coeff
+                    else:
+                        return None
+                elif t == 3:
+                    bl = bl_0
+                    i  = idx_cdag_0
+                    j  = idx_c_0
+                    U_tilde = -coeff
+        return [bl,i,j,U_tilde]
+                        
+            
+    def jacobi(self,x,solve_params):
+        #setting up needed parameters
+        assert 'h_int' in solve_params
+        if 'n_s' not in solve_params: solve_params['n_s'] = 1
+        if 'delta' not in solve_params: solve_params['delta'] = 0.1
+        solve_params.pop('delta')
+        n_s = solve_params['n_s']
+        h_int = solve_params['h_int']
+        gf_struct = self.constr_params['gf_struct']
+        n_terms = len(list(h_int))
+        _alpha = x.reshape(n_terms, 2, 2, n_s)            
+        _params = solve_params.copy()
+        _params['alpha'] = _alpha
+        _params.update(self.constr_params)
+        km = {}
+        for bl, g_bl in self.G0_iw:
+            self.G0_iw_inv[bl] << inverse(g_bl)
+            km[bl] = make_zero_tail(g_bl, 2)
+            km[bl][1] = np.eye(g_bl.target_shape[0])
+        #Set trivial, diagonal derivation:
+        jac = np.zeros((len(x),len(x)))
+        for i in range(len(x)):
+            jac[i,i] = -1.0
+        # Update the shifted Green function
+        self.prepare_G0_shift_iw(**_params)
+        bl= gf_struct[0][0]
+        G =  self.G0_shift_iw[bl][0,0].copy()
+        #create List with needed indices
+        L  = []
+        for n, (term, coeff) in enumerate(h_int):
+            bl_0, u_cdag_0 = term[0][1]
+            bl_1, u_cdag_1 = term[1][1]
+            bl_1, u_c_1 = term[2][1]
+            bl_0, u_c_0 = term[3][1]
+            idx_cdag_0 = dict(gf_struct)[bl_0].index(u_cdag_0)
+            idx_cdag_1 = dict(gf_struct)[bl_1].index(u_cdag_1)
+            idx_c_1 = dict(gf_struct)[bl_1].index(u_c_1)
+            idx_c_0 = dict(gf_struct)[bl_0].index(u_c_0)                              
+            L.append([bl_0,idx_c_0,idx_cdag_0,n,0])
+            if bl_0 == bl_1:
+                L.append([bl_0,idx_c_0,idx_cdag_1,n,1])
+                L.append([bl_1,idx_c_0,idx_cdag_1,n,2])
+            L.append([bl_1,idx_c_1,idx_cdag_1,n,3])
+        for K in L:
+            i = K[3]*4+K[4]
+            sigma = K[0]
+            h = K[1]
+            l = K[2]
+            for n in range(n_terms):
+                for t in range(4):
+                    c = self.convert_alpha_to_ij(h_int,gf_struct,n,t)
+                    if c == None:
+                        continue;
+                    else:
+                        [sigma_2,z1,z2,U_tilde] = c
+                        j = 4*n+t
+                        G.zero()
+                        if sigma == sigma_2:
+                            G = -self.G0_shift_iw[sigma_2][h,z1]*self.G0_shift_iw[sigma_2][z2,l]*U_tilde
+                    jac[i,j] += np.real(G.density())
+        return jac
+
+
+    
     def solve(self, **solve_params):
         """
         Solve the impurity problem.
@@ -65,14 +217,14 @@ class Solver(SolverCore):
 
         assert 'h_int' in solve_params
         assert 'n_cycles' in solve_params
-
+        self.eval_count = 0
         delta = solve_params.pop('delta', 0.1)
         if 'n_s' not in solve_params: solve_params['n_s'] = 1
 
         h_int = solve_params['h_int']
         gf_struct = self.constr_params['gf_struct']
         n_s = solve_params['n_s']
-         
+        
         if 'alpha' not in solve_params:
             # --------- Determine the alpha tensor from SC Hartree Fock ----------
             mpi_print("Determine alpha-tensor")
@@ -90,82 +242,27 @@ class Solver(SolverCore):
                 self.G0_iw_inv[bl] << inverse(g_bl)
                 km[bl] = make_zero_tail(g_bl, 2)
                 km[bl][1] = np.eye(g_bl.target_shape[0])
-
+                
+            if 'use_jacobi' not in solve_params: solve_params['use_jacobi'] = True
+            use_jacobi = solve_params['use_jacobi']
+            solve_params.pop('use_jacobi')
             # The numer of terms in h_int determines the leading dimension of alpha
             n_terms = len(list(h_int))
 
-            # Make sure that n_s parameter is compatible with automatic alpha
-            n_s = params_kw.get('n_s', 1)
-            if(all(any(s in bl for s in ['up', 'dn', 'do']) for bl, idxlst in gf_struct)):
-                assert n_s in [1, 2], "For spinfull systems the automatic alpha determination requires n_s to be either 1 or 2"
-            else:
-                assert n_s == 1, "For spinless systems the automatic alpha determination requires n_s to be 1"
-
-            # Find the root of this function
-            def f(alpha_vec):
-                self.eval_count = self.eval_count + 1
-
-                # Reshape input and prepare params
-                _alpha = alpha_vec.reshape(n_terms, 2, 2, n_s)
-                _params = solve_params.copy()
-                _params['alpha'] = _alpha
-                _params.update(self.constr_params)
-
-                # Update the shifted Green function
-                self.prepare_G0_shift_iw(**_params)
-
-                # Calculate the shifted density
-                _G0_shift_dens = { bl: g_bl.density(km[bl]).real for bl, g_bl in self.G0_shift_iw }
-
-                # Evaluate the violation of the self-consistency condition
-                _res = []
-                for n, (term, coeff) in enumerate(h_int):
-
-                    bl_0, u_cdag_0 = term[0][1]
-                    bl_1, u_cdag_1 = term[1][1]
-                    bl_1, u_c_1 = term[2][1]
-                    bl_0, u_c_0 = term[3][1]
-
-                    idx_cdag_0 = dict(gf_struct)[bl_0].index(u_cdag_0)
-                    idx_cdag_1 = dict(gf_struct)[bl_1].index(u_cdag_1)
-                    idx_c_1 = dict(gf_struct)[bl_1].index(u_c_1)
-                    idx_c_0 = dict(gf_struct)[bl_0].index(u_c_0)
-            
-                    _res.append(_G0_shift_dens[bl_0][idx_c_0,idx_cdag_0] - _alpha[n,0,0,0])
-                    _res.append(_G0_shift_dens[bl_1][idx_c_1,idx_cdag_1] - _alpha[n,1,1,0])
-                    # _res.append(G0_shift_dens[bl_0][idx_c_0,idx_cdag_0] + (1 if idx_c_0 == idx_cdag_0 else 0) - alpha[n,0,0,0] + sign(coeff) * delta)
-                    # _res.append(G0_shift_dens[bl_1][idx_c_1,idx_cdag_1] + (1 if idx_c_1 == idx_cdag_1 else 0) - alpha[n,1,1,0] - delta)
-
-                    if bl_0 == bl_1:
-                        _res.append(_G0_shift_dens[bl_0][idx_c_1,idx_cdag_0] - _alpha[n,0,1,0])
-                        _res.append(_G0_shift_dens[bl_0][idx_c_0,idx_cdag_1] - _alpha[n,1,0,0])
-                        # _res.append(G0_shift_dens[bl_0][idx_c_1,idx_cdag_0] + (1 if idx_c_1 == idx_cdag_0 else 0) - alpha[n,0,1,0] - delta)
-                        # _res.append(G0_shift_dens[bl_0][idx_c_0,idx_cdag_1] + (1 if idx_c_0 == idx_cdag_1 else 0) - alpha[n,1,0,0] - sign(coeff) * delta)
-                    else:
-                        _res.append(_alpha[n,0,1,0])
-                        _res.append(_alpha[n,1,0,0])
-            
-                # mpi_print("Function norm " + str(np.linalg.norm(_res)))
-                return np.array(_res)
-            
             # Calculate initial alpha guess from G0_iw.density(km)
             alpha_init = np.zeros((n_terms, 2, 2, n_s))
             G0_dens = { bl: g_bl.density(km[bl]).real for bl, g_bl in self.G0_iw }
             for n, (term, coeff) in enumerate(h_int):
-
                 bl_0, u_cdag_0 = term[0][1]
                 bl_1, u_cdag_1 = term[1][1]
                 bl_1, u_c_1 = term[2][1]
                 bl_0, u_c_0 = term[3][1]
-
                 idx_cdag_0 = dict(gf_struct)[bl_0].index(u_cdag_0)
                 idx_cdag_1 = dict(gf_struct)[bl_1].index(u_cdag_1)
                 idx_c_1 = dict(gf_struct)[bl_1].index(u_c_1)
-                idx_c_0 = dict(gf_struct)[bl_0].index(u_c_0)
-            
+                idx_c_0 = dict(gf_struct)[bl_0].index(u_c_0)     
                 alpha_init[n,0,0,0] = G0_dens[bl_0][idx_c_0,idx_cdag_0]
                 alpha_init[n,1,1,0] = G0_dens[bl_1][idx_c_1,idx_cdag_1]
-
                 if bl_0 == bl_1:
                     alpha_init[n,0,1,0] = G0_dens[bl_0][idx_c_1,idx_cdag_0]
                     alpha_init[n,1,0,0] = G0_dens[bl_0][idx_c_0,idx_cdag_1]
@@ -173,19 +270,28 @@ class Solver(SolverCore):
                     alpha_init[n,0,1,0] = 0
                     alpha_init[n,1,0,0] = 0
 
+            
             # mpi_print("Init Alpha: " + str(alpha_init[...,0]))
             alpha_vec_init = alpha_init.reshape(4 * n_terms * n_s)
             alpha = alpha_init
 
+            
             if mpi.is_master_node():
                 # Find alpha on master_node
+
+             
                 self.eval_count = 0
-                root_finder = root(f, alpha_vec_init, method="hybr", tol=1e-5)
-                # mpi_print("Number of calls: " + str(self.eval_count))
-                
+                if use_jacobi == True:
+                    mpi_print("Using Jacobi-Matrix for root search")
+                    root_finder = root(self.f, alpha_vec_init,args=(solve_params),jac=self.jacobi,method="hybr")
+                else:
+                    root_finder = root(self.f, alpha_vec_init,args=(solve_params),method="hybr")
+
+                    
                 # Reshape result, Implement Fallback solution if unsuccessful
                 if root_finder['success']:
                     alpha = root_finder['x'].reshape(n_terms, 2, 2, n_s)
+                    solve_params['alpha'] = alpha
                 else:
                     mpi_print("Could not determine alpha, falling back to G0_iw.density()")
 
@@ -202,7 +308,5 @@ class Solver(SolverCore):
             mpi_print(str(alpha[...,0]))
             solve_params['alpha'] = alpha
 
-        # Call the core solver's solve routine
         solve_status = SolverCore.solve(self, **solve_params)
-
         return solve_status
