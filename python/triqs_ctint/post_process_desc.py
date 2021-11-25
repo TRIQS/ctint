dc3618910af67e7c097a4d7eb1b011158a96d6b3
Henri Menke <henri@henrimenke.de>
2021-11-25 12:24:34 +0100

    Fix runaway memory in G2c_from_M4
    Co-authored-by: Marcel Klett <m.klett@fkf.mpg.de>
    Co-authored-by: Nils Wentzell <Wentzell@users.noreply.github.com>

--- python/triqs_ctint/post_process_desc.py
+++ python/triqs_ctint/post_process_desc.py
@@ -6,7 +6,7 @@ from cpp2py.wrap_generator import *
 module = module_(full_name = "post_process", doc = r"The TRIQS ctint postprocess functionality", app_name = "triqs_ctint")
 
 # Imports
-module.add_imports(*['triqs.gf', 'triqs.operators'])
+module.add_imports(*['triqs.gf', 'triqs.gf.meshes', 'triqs.operators', 'triqs.utility.mpi'])
 
 # Add here all includes
 module.add_include("triqs_ctint/post_process.hpp")
@@ -15,10 +15,9 @@ module.add_include("triqs_ctint/post_process.hpp")
 module.add_preamble("""
 #include <cpp2py/converters/complex.hpp>
 #include <cpp2py/converters/pair.hpp>
+#include <cpp2py/converters/std_array.hpp>
 #include <cpp2py/converters/string.hpp>
-#include <cpp2py/converters/variant.hpp>
 #include <cpp2py/converters/vector.hpp>
-#include <triqs/cpp2py_converters/arrays.hpp>
 #include <triqs/cpp2py_converters/gf.hpp>
 #include <triqs/cpp2py_converters/operators_real_complex.hpp>
 #include <triqs/cpp2py_converters/real_or_complex.hpp>
@@ -27,13 +26,13 @@ using namespace triqs_ctint;
 """)
 
 
-module.add_function ("triqs_ctint::chi4_iw_t triqs_ctint::G2c_from_M4 (chi4_iw_t::view_type M4_iw, triqs_ctint::g_iw_cv_t M_iw, triqs_ctint::g_iw_cv_t G0_iw)", doc = r"""Calculate the connected part of the two-particle Green function from M4_iw and M_iw""")
+module.add_function ("triqs_ctint::chi4_iw_t triqs_ctint::G2c_from_M4 (chi4_iw_t::view_type M4_iw, triqs_ctint::g_iw_cv_t M_iw, triqs_ctint::g_iw_cv_t G0_iw, mpi::communicator comm)", doc = r"""Calculate the connected part of the two-particle Green function from M4_iw and M_iw""")
 
 module.add_function ("triqs_ctint::chi4_iw_t triqs_ctint::F_from_G2c (chi4_iw_t::view_type G2c_iw, triqs_ctint::g_iw_cv_t G_iw)", doc = r"""Calculate the vertex function :math:`F` from G2c_iw and G_iw""")
 
 module.add_function ("triqs_ctint::chi4_iw_t triqs_ctint::G2_from_G2c (chi4_iw_t::view_type G2c_iw, triqs_ctint::g_iw_cv_t G_iw)", doc = r"""Calculate the two-particle Green function from G2c_iw and G_iw""")
 
-module.add_function ("triqs_ctint::chi4_iw_t triqs_ctint::chi_tilde_ph_from_G2c (chi4_iw_t::view_type G2c_iw, triqs_ctint::g_iw_cv_t G_iw, triqs::hilbert_space::gf_struct_t gf_struct)", doc = r"""Calculate the generalized ph susceptibility from G2c_iw and G_iw""")
+module.add_function ("triqs_ctint::chi4_iw_t triqs_ctint::chi_tilde_ph_from_G2c (chi4_iw_t::view_type G2c_iw, triqs_ctint::g_iw_cv_t G_iw, triqs::gfs::gf_struct_t gf_struct)", doc = r"""Calculate the generalized ph susceptibility from G2c_iw and G_iw""")
 
 module.add_function ("triqs_ctint::chi3_iw_t triqs_ctint::chi3_from_M3_PP (triqs_ctint::chi3_iw_cv_t M3_iw, triqs_ctint::g_iw_cv_t M_iw, triqs_ctint::g_iw_cv_t G0_iw, triqs_ctint::block_matrix_t dens_G, triqs_ctint::block_matrix_t M_hartree)", doc = r"""""")
 
@@ -43,9 +42,9 @@ module.add_function ("triqs_ctint::chi2_tau_t triqs_ctint::chi2_from_chi2_conn_P
 
 module.add_function ("triqs_ctint::chi2_tau_t triqs_ctint::chi2_from_chi2_conn_PH (triqs_ctint::chi2_tau_cv_t chi2_conn_tau, triqs_ctint::g_iw_cv_t G_iw, triqs_ctint::block_matrix_t dens_G)", doc = r"""""")
 
-module.add_function ("gf<triqs::gfs::imtime, triqs::gfs::matrix_valued> triqs_ctint::chiAB_from_chi2_PP (triqs_ctint::chi2_tau_cv_t chi2pp_tau, triqs::hilbert_space::gf_struct_t gf_struct, std::vector<many_body_operator> A_op_vec, std::vector<many_body_operator> B_op_vec)", doc = r"""""")
+module.add_function ("gf<triqs::mesh::imtime, triqs::gfs::matrix_valued> triqs_ctint::chiAB_from_chi2_PP (triqs_ctint::chi2_tau_cv_t chi2pp_tau, triqs::gfs::gf_struct_t gf_struct, std::vector<many_body_operator> A_op_vec, std::vector<many_body_operator> B_op_vec)", doc = r"""""")
 
-module.add_function ("gf<triqs::gfs::imtime, triqs::gfs::matrix_valued> triqs_ctint::chiAB_from_chi2_PH (triqs_ctint::chi2_tau_cv_t chi2ph_tau, triqs::hilbert_space::gf_struct_t gf_struct, std::vector<many_body_operator> A_op_vec, std::vector<many_body_operator> B_op_vec)", doc = r"""""")
+module.add_function ("gf<triqs::mesh::imtime, triqs::gfs::matrix_valued> triqs_ctint::chiAB_from_chi2_PH (triqs_ctint::chi2_tau_cv_t chi2ph_tau, triqs::gfs::gf_struct_t gf_struct, std::vector<many_body_operator> A_op_vec, std::vector<many_body_operator> B_op_vec)", doc = r"""""")
 
 module.add_function ("triqs_ctint::chi2_tau_t triqs_ctint::chi2_conn_from_M3_PP (triqs_ctint::chi3_tau_t M3pp_tau, triqs_ctint::chi2_tau_t M3pp_delta, triqs_ctint::g_iw_cv_t M_iw, triqs_ctint::g_iw_cv_t G0_iw, triqs_ctint::g_tau_cv_t M_tau, triqs_ctint::block_matrix_t M_hartree, triqs_ctint::g_tau_cv_t G0_tau)", doc = r"""""")
 
