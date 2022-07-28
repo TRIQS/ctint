51d01519774ba47cd55509ec46f8db0b61d8c582
Henri Menke <henri@henrimenke.de>
2022-07-28 12:55:20 +0200

    Record total number of measures in average_sign
    Co-authored-by: Marcel Klett <m.klett@fkf.mpg.de>

--- c++/triqs_ctint/measures/average_sign.cpp
+++ c++/triqs_ctint/measures/average_sign.cpp
@@ -3,8 +3,9 @@
 namespace triqs_ctint::measures {
 
   average_sign::average_sign(params_t const &, qmc_config_t const &qmc_config_, container_set *results)
-     : average_sign_(results->average_sign), qmc_config(qmc_config_) {
+     : average_sign_(results->average_sign), nmeasures(results->nmeasures), qmc_config(qmc_config_) {
     average_sign_ = 0.0;
+    nmeasures     = 0;
   }
 
   void average_sign::accumulate(mc_weight_t sign) {
@@ -32,6 +33,7 @@ namespace triqs_ctint::measures {
     average_sign_ = mpi::all_reduce(average_sign_, comm);
     count         = mpi::all_reduce(count, comm);
     average_sign_ = average_sign_ / count;
+    nmeasures     = count;
   }
 
   std::string average_sign::report() const {
