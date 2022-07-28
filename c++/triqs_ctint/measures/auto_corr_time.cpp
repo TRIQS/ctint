dcaf41de834f4af2223ab0a229481850df0b2c6d
Henri Menke <henri@henrimenke.de>
2022-07-28 11:45:47 +0200

    In estimate of auto_corr_time use also perturbation order
    (ported from https://github.com/TRIQS/cthyb/commit/fd5e7fd0d351aa8622d769508ed6edb04bebc3d4)
    
    Co-authored-by: Marcel Klett <m.klett@fkf.mpg.de>
    Co-authored-by: Nils Wentzell <Wentzell@users.noreply.github.com>

--- c++/triqs_ctint/measures/auto_corr_time.cpp
+++ c++/triqs_ctint/measures/auto_corr_time.cpp
@@ -2,22 +2,31 @@
 
 namespace triqs_ctint::measures {
 
-  auto_corr_time::auto_corr_time(params_t const &, qmc_config_t const &, container_set *results) : auto_corr_time_(results->auto_corr_time) {}
+  auto_corr_time::auto_corr_time(params_t const &, qmc_config_t const &qmc_config, container_set *results)
+     : qmc_config(qmc_config), auto_corr_time_(results->auto_corr_time) {}
 
-  void auto_corr_time::accumulate(mc_weight_t sign) { log_acc << sign; }
+  void auto_corr_time::accumulate(mc_weight_t sign) {
+    log_accs[0] << sign;
+    log_accs[1] << qmc_config.perturbation_order();
+  }
 
   void auto_corr_time::collect_results(mpi::communicator const &comm) {
     using namespace triqs::stat;
 
-    auto [errs, counts] = log_acc.log_bin_errors_all_reduce(comm);
-
-    // Estimate auto-correlation time
     auto_corr_time_ = 0.0;
-    if (comm.rank() == 0 && errs[0] > 0) auto_corr_time_ = std::max(0.0, tau_estimate_from_errors(errs[int(0.7 * errs.size())], errs[0]));
-    mpi::broadcast(auto_corr_time_, comm, 0);
 
-    // Reset the accumulator
-    log_acc = {0.0, -1, 0};
+    for (auto &log_acc : log_accs) {
+      auto [errs, counts] = log_acc.log_bin_errors_all_reduce(comm);
+
+      // Estimate auto-correlation time
+      if (comm.rank() == 0 && errs[0] > 0) {
+        auto_corr_time_ = std::max(auto_corr_time_, tau_estimate_from_errors(errs[int(0.7 * errs.size())], errs[0]));
+      }
+
+      // Reset the accumulator
+      log_acc = {0.0, -1, 0};
+    }
+    mpi::broadcast(auto_corr_time_, comm, 0);
   }
 
 } // namespace triqs_ctint::measures
