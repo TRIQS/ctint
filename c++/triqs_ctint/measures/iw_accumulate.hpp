4f8db3fc50197d75f4cc2330fa414893b04005d1
Fikret Ardal <fikret.ardal@tum.de>
2025-03-13 02:07:39 +0300

    kernel_block updates inplace

--- c++/triqs_ctint/measures/iw_accumulate.hpp
+++ c++/triqs_ctint/measures/iw_accumulate.hpp
@@ -15,31 +15,12 @@ namespace triqs_ctint::measures {
       static_assert(outer_loop == 1, "outloop width 1 is supported.");
       static_assert(inner_loop == 1 or inner_loop == 2 or inner_loop == 4);
       static_assert(outer_loop == 1 or outer_loop == 2 or outer_loop == 4);
-//      if (true) {
-//        for (long i : range(bl1_size)) {
-//          for (long j : range(bl1_size)) {
-//            for (long k : range(bl2_size))
-//              for (long l : range(bl2_size)) {
-//                M4_view(i, j, k, l) += sign * M1_view_val(j, i) * M2_view(l, k);
-//                if constexpr (equal_sizes) { M4_view(i, j, k, l) -= sign * M1_view_if(l, i) * M2_view_if(j, k); }
-//              }
-//          }
-//        }
-//        return;
-//      }
       using value_t                    = std::complex<double>;
       using simd_t                     = fixed_size_simd<value_t, inner_loop>;
       constexpr size_t inner_simd_size = simd_t::size();
       const long M1_view_if_stride     = M1_view_if.strides()[0]; // C_layout, stride length between columns.
       const long M4_view_stride        = M4_view.strides()[2];    // C_layout 2nd fastest dimension
       const long M2_view_stride        = M2_view.strides()[0];    // C_layout 2nd fastest dimension.
-      simd_t sign_simd;
-      if constexpr (std::is_same_v<std::remove_cvref_t<decltype(sign)>, value_t>) {
-        sign_simd = simd_t(sign);
-      } else if constexpr (std::is_same_v<std::remove_cvref_t<decltype(sign)>, double>) {
-
-        sign_simd = simd_t(value_t(sign, 0)); // TODO: Improve this by adding operator*(const Scalar& v) in complex simds.
-      }
       std::array<simd_t, inner_simd_size> M4_block, M2_view_block;
       for (long i : range(bl1_size)) {
         for (long j : range(bl1_size)) {
@@ -58,14 +39,14 @@ namespace triqs_ctint::measures {
                 M2_view_block[m].load_unaligned(&M2_view(l + m, k));
               }
               // Transpose the M2_view. Because 'l' index is the column index of M2_view.
-              M2_view_block = simd::kernel_transpose(M2_view_block);
+              simd::kernel_transpose(M2_view_block);
               for (int m = 0; m < inner_simd_size; ++m) { M4_block[m] = simd::fma_add(M1val_sign_simd, M2_view_block[m], M4_block[m]); }
 
               if constexpr (equal_sizes) {
                 //Since l is column. We need to call gather with correct stride. TODO: If you vectorize i as well we can call normal loads and then transpose.
                 simd_t tmp = simd::gather<simd_t>(&M1_view_if(l, i), M1_view_if_stride);
                 for (int m = 0; m < inner_simd_size; ++m) {
-                  M4_block[m] = simd::fma_nadd(sign_simd * tmp, simd_t(M2_view_if(j, k + m)), M4_block[m]);
+                  M4_block[m] = simd::fma_nadd(tmp * sign, simd_t(M2_view_if(j, k + m)), M4_block[m]);
                 }
               }
               for (int m = 0; m < inner_simd_size; ++m) { M4_block[m].store_unaligned(&M4_view(i, j, k + m, l)); }
@@ -79,7 +60,7 @@ namespace triqs_ctint::measures {
                 simd_t tmp3(M1_view_if(l, i));
                 simd_t tmp4;
                 tmp4.load_unaligned(&M2_view_if(j, k));
-                tmp = simd::fma_nadd(sign_simd * tmp3, tmp4, tmp);
+                tmp = simd::fma_nadd(tmp3 * sign, tmp4, tmp);
               }
               simd::scatter(tmp, &M4_view(i, j, k, l), M4_view_stride);
             }
@@ -91,7 +72,7 @@ namespace triqs_ctint::measures {
               tmp.load_unaligned(&M4_view(i, j, k, l));
               tmp = simd::fma_add(M1val_sign_simd, simd::gather<simd_t>(&M2_view(l, k), M2_view_stride), tmp);
               if constexpr (equal_sizes) {
-                tmp = simd::fma_nadd(sign_simd * simd::gather<simd_t>(&M1_view_if(l, i), M1_view_if_stride), simd_t(M2_view_if(j, k)), tmp);
+                tmp = simd::fma_nadd(simd::gather<simd_t>(&M1_view_if(l, i), M1_view_if_stride) * sign, simd_t(M2_view_if(j, k)), tmp);
               }
               tmp.store_unaligned(&M4_view(i, j, k, l));
             }
@@ -112,9 +93,9 @@ namespace triqs_ctint::measures {
                                       const basic_array_view<const std::complex<double>, 2, C_layout> &M2_view_if,
                                       basic_array_view<std::complex<double>, 4, C_stride_layout> &M4_view) {
     constexpr size_t max_width = native_simd<std::complex<double>>::size();
-    if (bl2_size > max_width or max_width == 1) { //NOLINT
+    if (bl2_size >= max_width or max_width == 1) { //NOLINT
       _impl_process_vectorized_loop<1, max_width, equal_sizes>(sign, bl1_size, bl2_size, M1_view_val, M1_view_if, M2_view, M2_view_if, M4_view);
-    } else if (bl2_size > max_width / 2 or max_width / 2 == 1) { //NOLINT
+    } else if (bl2_size >= max_width / 2 or max_width / 2 == 1) { //NOLINT
       _impl_process_vectorized_loop<1, max_width / 2, equal_sizes>(sign, bl1_size, bl2_size, M1_view_val, M1_view_if, M2_view, M2_view_if, M4_view);
     } else {
       _impl_process_vectorized_loop<1, 1, equal_sizes>(sign, bl1_size, bl2_size, M1_view_val, M1_view_if, M2_view, M2_view_if, M4_view);
