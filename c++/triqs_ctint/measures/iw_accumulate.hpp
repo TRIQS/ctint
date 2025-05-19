7b54f04fa8e0b46b28e840f0807bb9a84348d780
Marco Barbone <mbarbone@flatironinstitute.org>
2025-05-19 17:46:24 -0400

    some optimizations

--- c++/triqs_ctint/measures/iw_accumulate.hpp
+++ c++/triqs_ctint/measures/iw_accumulate.hpp
@@ -23,7 +23,7 @@ namespace triqs_ctint::measures {
       } else {
         return N;
       }
-    };
+    }
 
     template <class T, auto N> constexpr auto max_simd_width() {
       // finds the smallest simd width that can handle N elements
@@ -33,7 +33,7 @@ namespace triqs_ctint::measures {
       } else {
         return N;
       }
-    };
+    }
 
     constexpr auto min_width = min_simd_width<std::complex<double>, 1>();
     constexpr auto max_width = max_simd_width<std::complex<double>, 128>();
@@ -49,70 +49,64 @@ namespace triqs_ctint::measures {
     static_assert(!std::is_void_v<make_sized_batch_t<std::complex<double>, max_width>>, "failed to create max_width complex batch");
 
     // === Fused, vectorized version for bl1_batch == bl2_batch ===
-    template <int batch_size, typename M1a_t, typename M2a_t, typename M1b_t, typename M2b_t, typename M4a_t>
-    void process_inner_loop_fused(mc_weight_t sign, const M1a_t &M1a, const M2a_t &M2a, const M1b_t &M1b, const M2b_t &M2b, M4a_t &M4a,
-                                  auto bl1_size, auto bl2_size, auto bl1, auto bl2) {
-      using batch_t = make_sized_batch_t<std::complex<double>, std::max(std::min(batch_size, max_width), min_width)>;
+    template <int batch_size, bool diagonal>
+    void process_inner_loop_fused(const mc_weight_t sign, const auto &M1a, const auto &M2a, const auto &M1b, const auto &M2b, auto &M4a,
+                                  const auto bl1_size, const auto bl2_size) noexcept {
+      using batch_t                   = make_sized_batch_t<std::complex<double>, std::max(std::min(batch_size, max_width), min_width)>;
+      static constexpr auto simd_size = batch_t::size;
 
-      for (auto i : range(bl1_size)) {
-        for (auto j : range(bl1_size)) {
-          const auto M1_v = batch_t{M1a(j, i) * sign};
+      for (const auto i : range(bl1_size))
+        for (const auto j : range(bl1_size)) {
+          const auto M1a_s = M1a(j, i) * sign;
+          const auto M1a_v = batch_t{M1a_s};
 
-          for (auto k : range(bl2_size)) {
-            auto *M4_ptr  = &M4a(i, j, k, 0);
-            auto *M2a_ptr = M2a.data() + k * bl2_size;
+          for (const auto k : range(bl2_size)) {
+            auto *const RESTRICT M4_ptr = &M4a(i, j, k, 0);
+            const auto *const M2a_ptr   = M2a.data() + k * bl2_size;
 
-            const auto V   = batch_t::size;
-            const auto end = bl2_size & ~(V - 1);
+            const auto end = bl2_size & ~(simd_size - 1);
             auto idx       = 0;
 
-            if (bl1 == bl2) {
-              const auto M2s_v = batch_t{M2b(j, k) * sign};
-              auto *M1b_ptr    = &M1b(0, i);
+            if constexpr (diagonal) {
+              const auto M2b_s    = M2b(j, k) * sign;
+              const auto M2b_v    = batch_t{M2b_s};
+              auto *const M1b_ptr = &M1b(0, i);
 
               // fused FMA + FNMA
-              for (; idx < end; idx += V) {
-                auto m4  = batch_t::load_unaligned(M4_ptr + idx);
-                auto m2a = batch_t::load_unaligned(M2a_ptr + idx);
-                auto m1b = batch_t::load_unaligned(M1b_ptr + idx);
+              for (; idx < end; idx += simd_size) {
+                const auto m4  = batch_t::load_unaligned(M4_ptr + idx);
+                const auto m2a = batch_t::load_unaligned(M2a_ptr + idx);
+                const auto m1b = batch_t::load_unaligned(M1b_ptr + idx);
 
-                auto tmp    = xsimd::fma(M1_v, m2a, m4);
-                auto result = xsimd::fnma(M2s_v, m1b, tmp);
+                const auto tmp    = xsimd::fma(M1a_v, m2a, m4);
+                const auto result = xsimd::fnma(M2b_v, m1b, tmp);
 
                 result.store_unaligned(M4_ptr + idx);
               }
-              for (; idx < bl2_size; ++idx) {
-                auto &m4 = M4_ptr[idx];
-                m4       = xsimd::fnma(M2b(j, k) * sign, M1b(idx, i), xsimd::fma(M1a(j, i) * sign, M2a_ptr[idx], m4));
-              }
+              for (; idx < bl2_size; ++idx) { M4_ptr[idx] = xsimd::fnma(M2b_s, M1b(idx, i), xsimd::fma(M1a_s, M2a_ptr[idx], M4_ptr[idx])); }
             } else {
               // only FMA for M1a*M2a
-              for (; idx < end; idx += V) {
-                auto m4  = batch_t::load_unaligned(M4_ptr + idx);
-                auto m2a = batch_t::load_unaligned(M2a_ptr + idx);
-
-                auto result = xsimd::fma(M1_v, m2a, m4);
+              for (; idx < end; idx += simd_size) {
+                const auto m4     = batch_t::load_unaligned(M4_ptr + idx);
+                const auto m2a    = batch_t::load_unaligned(M2a_ptr + idx);
+                const auto result = xsimd::fma(M1a_v, m2a, m4);
                 result.store_unaligned(M4_ptr + idx);
               }
-              for (; idx < bl2_size; ++idx) {
-                auto &m4 = M4_ptr[idx];
-                m4       = xsimd::fma(M1a(j, i) * sign, M2a_ptr[idx], m4);
-              }
+              for (; idx < bl2_size; ++idx) { M4_ptr[idx] = xsimd::fma(M1a_s, M2a_ptr[idx], M4_ptr[idx]); }
             }
           }
         }
-      }
     }
 
     // === Fallback version for other batch sizes ===
-    template <int bl1_batch, int bl2_batch, typename M1a_t, typename M2a_t, typename M1b_t, typename M2b_t, typename M4a_t>
-    void process_inner_loop_fallback(mc_weight_t sign, const M1a_t &M1a, const M2a_t &M2a, const M1b_t &M1b, const M2b_t &M2b, M4a_t &M4a,
-                                     auto bl1_size, auto bl2_size, auto bl1, auto bl2) {
+    template <int bl1_batch, int bl2_batch, bool diagonal>
+    void process_inner_loop_fallback(const mc_weight_t sign, const auto &M1a, const auto &M2a, const auto &M1b, const auto &M2b, auto &M4a,
+                                     const auto bl1_size, const auto bl2_size) noexcept {
       using batch1_t = make_sized_batch_t<std::complex<double>, std::max(std::min(bl1_batch, max_width), min_width)>;
       using batch2_t = make_sized_batch_t<std::complex<double>, std::max(std::min(bl2_batch, max_width), min_width)>;
 
-      for (auto i : range(bl1_size)) {
-        for (auto j : range(bl1_size)) {
+      for (const auto i : range(bl1_size))
+        for (const auto j : range(bl1_size)) {
           uint64_t index       = 0;
           const auto M1val     = M1a(j, i) * sign;
           const auto bl2square = bl2_size * bl2_size;
@@ -123,17 +117,17 @@ namespace triqs_ctint::measures {
             const auto truncated_size = bl2square & -(batch1_t::size);
             for (; index < truncated_size; index += batch1_t::size) {
               auto *const RESTRICT m4_ptr = &M4a(i, j, 0, 0) + index;
-              auto batch                  = batch1_t::load_unaligned(m4_ptr);
-              auto M2_batch               = batch1_t::load_unaligned(M2a.data() + index);
-              auto result                 = xsimd::fma(M1_v, M2_batch, batch);
+              const auto batch            = batch1_t::load_unaligned(m4_ptr);
+              const auto M2_batch         = batch1_t::load_unaligned(M2a.data() + index);
+              const auto result           = xsimd::fma(M1_v, M2_batch, batch);
               result.store_unaligned(m4_ptr);
             }
           }
           for (; index < bl2square; ++index) { (&M4a(i, j, 0, 0))[index] = xsimd::fma(M1val, M2a.data()[index], (&M4a(i, j, 0, 0))[index]); }
 
           // M2b * M1b update only on diagonal blocks
-          if (bl1 == bl2) {
-            for (auto k : range(bl2_size)) {
+          if constexpr (diagonal) {
+            for (const auto k : range(bl2_size)) {
               index             = 0;
               const auto M2sval = M2b(j, k) * sign;
               if constexpr (bl2_batch >= min_width) {
@@ -141,9 +135,9 @@ namespace triqs_ctint::measures {
                 const auto truncated_size = bl2_size & -(batch2_t::size);
                 for (; index < truncated_size; index += batch2_t::size) {
                   auto *const RESTRICT m4_ptr = &M4a(i, j, k, index);
-                  auto batch                  = batch2_t::load_unaligned(m4_ptr);
-                  auto M1_batch               = batch2_t::load_unaligned(&M1b(index, i));
-                  auto result                 = xsimd::fnma(M2s_v, M1_batch, batch);
+                  const auto batch            = batch2_t::load_unaligned(m4_ptr);
+                  const auto M1_batch         = batch2_t::load_unaligned(&M1b(index, i));
+                  const auto result           = xsimd::fnma(M2s_v, M1_batch, batch);
                   result.store_unaligned(m4_ptr);
                 }
               }
@@ -151,22 +145,27 @@ namespace triqs_ctint::measures {
             }
           }
         }
-      }
     }
 
     // === Wrapper that dispatches to fused or fallback ===
-    template <int bl1_batch, int bl2_batch, typename M1a_t, typename M2a_t, typename M1b_t, typename M2b_t, typename M4a_t>
-    void process_inner_loop(mc_weight_t sign, const M1a_t &M1a, const M2a_t &M2a, const M1b_t &M1b, const M2b_t &M2b, M4a_t &M4a,
-                            auto bl1_size, auto bl2_size, auto bl1, auto bl2) {
-      if constexpr (bl1_batch == bl2_batch && bl1_batch >= min_width) {
-        process_inner_loop_fused<bl1_batch>(sign, M1a, M2a, M1b, M2b, M4a, bl1_size, bl2_size, bl1, bl2);
+    template <int bl1_batch, int bl2_batch>
+    void process_inner_loop(const mc_weight_t sign, const auto &M1a, const auto &M2a, const auto &M1b, const auto &M2b, auto &M4a,
+                            const auto bl1_size, const auto bl2_size, const auto bl1, const auto bl2) noexcept {
+      if (bl1 == bl2)
+        if constexpr (bl1_batch == bl2_batch) {
+          return process_inner_loop_fused<bl1_batch, true>(sign, M1a, M2a, M1b, M2b, M4a, bl1_size, bl2_size);
+        } else {
+          return process_inner_loop_fallback<bl1_batch, bl2_batch, true>(sign, M1a, M2a, M1b, M2b, M4a, bl1_size, bl2_size);
+        }
+      if constexpr (bl1_batch == bl2_batch) {
+        return process_inner_loop_fused<bl1_batch, false>(sign, M1a, M2a, M1b, M2b, M4a, bl1_size, bl2_size);
       } else {
-        process_inner_loop_fallback<bl1_batch, bl2_batch>(sign, M1a, M2a, M1b, M2b, M4a, bl1_size, bl2_size, bl1, bl2);
+        return process_inner_loop_fallback<bl1_batch, bl2_batch, false>(sign, M1a, M2a, M1b, M2b, M4a, bl1_size, bl2_size);
       }
     }
 
-    const auto iw4_accumulate_kernel = []<int bl1_batch, int bl2_batch>(mc_weight_t sign, const auto &M, auto &M4_iw, const auto bl1,
-                                                                        const auto bl2) {
+    constexpr auto iw4_accumulate_kernel = []<int bl1_batch, int bl2_batch>(mc_weight_t sign, const auto &M, auto &M4_iw, const auto bl1,
+                                                                            const auto bl2) noexcept {
       //auto const &iw_mesh = std::get<0>(M4_iw(0, 0).mesh());
       auto &[iw_mesh, _, _] = M4_iw(0, 0).mesh();
       auto const M1         = M[bl1];
@@ -190,8 +189,8 @@ namespace triqs_ctint::measures {
       }
     };
 
-    const auto iw4ph_accumulate_kernel = []<int bl1_batch, int bl2_batch>(mc_weight_t sign, const auto &M, auto &M4_iw, const auto bl1,
-                                                                          const auto bl2) {
+    constexpr auto iw4ph_accumulate_kernel = []<int bl1_batch, int bl2_batch>(mc_weight_t sign, const auto &M, auto &M4_iw, const auto bl1,
+                                                                              const auto bl2) noexcept {
       //auto const &iW_mesh = std::get<0>(M4_iw(0, 0).mesh());
       //auto const &iw_mesh = std::get<1>(M4_iw(0, 0).mesh());
       auto const &[iW_mesh, iw_mesh, _] = M4_iw(0, 0).mesh();
@@ -215,8 +214,8 @@ namespace triqs_ctint::measures {
       }
     };
 
-    const auto iw4pp_accumulate_kernel = []<int bl1_batch, int bl2_batch>(mc_weight_t sign, const auto &M, auto &M4_iw, const auto bl1,
-                                                                          const auto bl2) {
+    constexpr auto iw4pp_accumulate_kernel = []<int bl1_batch, int bl2_batch>(mc_weight_t sign, const auto &M, auto &M4_iw, const auto bl1,
+                                                                              const auto bl2) noexcept {
       //auto const &iW_mesh = std::get<0>(M4_iw(0, 0).mesh());
       //auto const &iw_mesh = std::get<1>(M4_iw(0, 0).mesh());
       auto const &[iW_mesh, iw_mesh, _] = M4_iw(0, 0).mesh();
@@ -240,8 +239,8 @@ namespace triqs_ctint::measures {
       }
     };
 
-    const auto iw3pp_accumulate_kernel = []<int bl1_batch, int bl2_batch>(mc_weight_t sign, const auto &GM, auto &M3_iw, const auto bl1,
-                                                                          const auto bl2) {
+    constexpr auto iw3pp_accumulate_kernel = []<int bl1_batch, int bl2_batch>(mc_weight_t sign, const auto &GM, auto &M3_iw, const auto bl1,
+                                                                              const auto bl2) noexcept {
       auto const [iW_mesh, iw_mesh] = M3_iw(0, 0).mesh();
       auto const bl1_size           = GM[bl1].target_shape()[0];
       auto const bl2_size           = GM[bl2].target_shape()[0];
@@ -259,8 +258,8 @@ namespace triqs_ctint::measures {
       }
     };
 
-    const auto iw3ph_accumulate_kernel = []<int bl1_batch, int bl2_batch>(mc_weight_t sign, const auto &M, const auto &GMG, const auto &GM,
-                                                                          const auto &MG, auto &M3_iw, const auto bl1, const auto bl2) {
+    constexpr auto iw3ph_accumulate_kernel = []<int bl1_batch, int bl2_batch>(mc_weight_t sign, const auto &M, const auto &GMG, const auto &GM,
+                                                                              const auto &MG, auto &M3_iw, const auto bl1, const auto bl2) noexcept {
       auto const [iW_mesh, iw_mesh] = M3_iw(0, 0).mesh();
       auto const bl1_size           = M[bl1].target_shape()[0];
       auto const bl2_size           = M[bl2].target_shape()[0];
@@ -285,41 +284,35 @@ namespace triqs_ctint::measures {
 
   namespace simd {
 
-    template <auto kernel> void kernel_dispatch(const auto bl2_size, auto &&...args) {
+    template <auto kernel> void kernel_dispatch(const auto bl2_size, auto &&...args) noexcept {
       // Dispatch to the correct SIMD instruction width based on the size of the blocks
       // It will try to use the widest SIMD instruction available for the given block sizes
       // TODO: fold expressions might be an option to simplify the code
-      if (bl2_size >= 8) {
-        return kernel.template operator()<8, 8>(args...);
-      } else if (bl2_size >= 4) {
-        return kernel.template operator()<8, 4>(args...);
-      } else if (bl2_size >= 3) {
-        return kernel.template operator()<8, 2>(args...);
-      } else if (bl2_size >= 2) {
-        return kernel.template operator()<4, 2>(args...);
-      } else {
-        return kernel.template operator()<1, 1>(args...);
-      }
+      if (bl2_size >= 8) { return kernel.template operator()<8, 8>(std::forward<decltype(args)>(args)...); }
+      if (bl2_size >= 4) { return kernel.template operator()<8, 4>(std::forward<decltype(args)>(args)...); }
+      if (bl2_size >= 3) { return kernel.template operator()<8, 2>(std::forward<decltype(args)>(args)...); }
+      if (bl2_size >= 2) { return kernel.template operator()<4, 2>(std::forward<decltype(args)>(args)...); }
+      return kernel.template operator()<1, 1>(std::forward<decltype(args)>(args)...);
     }
 
-    void iw4_accumulate(mc_weight_t sign, const auto &M, auto &M4_iw, const auto bl1, const auto bl2, const auto bl2_size) {
+    void iw4_accumulate(mc_weight_t sign, const auto &M, auto &M4_iw, const auto bl1, const auto bl2, const auto bl2_size) noexcept {
       kernel_dispatch<iw4_accumulate_kernel>(bl2_size, sign, M, M4_iw, bl1, bl2);
     }
 
-    void iw4ph_accumulate(mc_weight_t sign, const auto &M, auto &M4_iw, const auto bl1, const auto bl2, const auto bl2_size) {
+    void iw4ph_accumulate(mc_weight_t sign, const auto &M, auto &M4_iw, const auto bl1, const auto bl2, const auto bl2_size) noexcept {
       kernel_dispatch<iw4ph_accumulate_kernel>(bl2_size, sign, M, M4_iw, bl1, bl2);
     }
 
-    void iw4pp_accumulate(mc_weight_t sign, const auto &M, auto &M4_iw, const auto bl1, const auto bl2, const auto bl2_size) {
+    void iw4pp_accumulate(mc_weight_t sign, const auto &M, auto &M4_iw, const auto bl1, const auto bl2, const auto bl2_size) noexcept {
       kernel_dispatch<iw4pp_accumulate_kernel>(bl2_size, sign, M, M4_iw, bl1, bl2);
     }
 
     void iw3ph_accumulate(mc_weight_t sign, const auto &M, const auto &GMG, const auto &GM, const auto &MG, auto &M4_iw, const auto bl1,
-                          const auto bl2, const auto bl2_size) {
+                          const auto bl2, const auto bl2_size) noexcept {
       kernel_dispatch<iw3ph_accumulate_kernel>(bl2_size, sign, M, GMG, GM, MG, M4_iw, bl1, bl2);
     }
 
-    void iw3pp_accumulate(mc_weight_t sign, const auto &M, auto &M4_iw, const auto bl1, const auto bl2, const auto bl2_size) {
+    void iw3pp_accumulate(mc_weight_t sign, const auto &M, auto &M4_iw, const auto bl1, const auto bl2, const auto bl2_size) noexcept {
       kernel_dispatch<iw3pp_accumulate_kernel>(bl2_size, sign, M, M4_iw, bl1, bl2);
     }
 
