a4b0d91334fa20adc27957aad389ed954efbe680
Thomas Hahn <thomas.hahn01@gmail.com>
2026-06-22 10:39:03 -0400

    Port to c2py + clair
    - remove cpp2py dependency and any references
    - prepare solver_core.hpp for clair + c2py
    - fetch c2py and build solver_core and post_process module with c2py_add_module
    - synchronize conf.py.in and autosummary templates with ctseg
    - update sphinx and python documentation
    - remove desc and rst files from python/triqs_ctint

---
+++ python/triqs_ctint/post_process.cpp
@@ -0,0 +1,14 @@
+#include <c2py/c2py.hpp>
+
+#include <triqs/operators.hpp>
+#include <triqs/mesh.hpp>
+#include <triqs/c2py_converters/gf.hpp>
+#include <triqs/c2py_converters/mesh.hpp>
+#include <triqs/c2py_converters/fundamental_operator_set.hpp>
+#include <triqs/c2py_converters/operators_real_complex.hpp>
+#include <triqs/c2py_converters/real_or_complex.hpp>
+#include <triqs/c2py_converters/arrays.hpp>
+
+#include <triqs_ctint/post_process.hpp>
+
+#include "post_process.wrap.cxx"
